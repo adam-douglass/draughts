f573d7e6dfb3306aa09543aa1debac33ae86e272
Adam <douglass@malloc.ca>
2020-01-02 12:06:30 -0500

    Create a setup.py for package builds

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name="draughts",
+    version="0.0",
+    packages=find_packages(),
+    
+    # metadata to display on PyPI
+    author="Adam Douglass",
+    author_email="douglass@malloc.ca",
+    description="Generates boilerplate for data objects.",
+    keywords="utility typechecking",
+    url="https://github.com/adam-douglass/draughts/"
+)

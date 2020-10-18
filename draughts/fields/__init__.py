e7cc9a3edbb1404ba1b0fbf8542149853cdfbe71
Adam Douglass <douglass@malloc.ca>
2020-10-17 20:49:30 -0400

    Add utility methods

--- draughts/fields/__init__.py
+++ draughts/fields/__init__.py
@@ -4,23 +4,29 @@ from .basic import Any
 from .pattern import PatternString, MD5, SHA256, PhoneNumber, MACAddress, PrivateIP, SSDeepHash, IP, \
     Domain, Email, SHA1, URI, URIPath
 from .complex import Compound
+from .bases import MultivaluedField, ProxyField
+from .multivalued import SimpleList
+from .complex import List as CompoundList
+from .bases import MultivaluedField, ProxyField
+from .multivalued import SimpleMapping
+from .complex import Mapping as CompoundMapping
 
 
 def List(field_type, **kwargs):
     """A helper "class" to choose the right list class for a given field."""
-    from .bases import MultivaluedField, ProxyField
-    from .multivalued import SimpleList
-    from .complex import List
     if isinstance(field_type, ProxyField):
-        return List(field_type, **kwargs)
+        return CompoundList(field_type, **kwargs)
     return SimpleList(field_type, **kwargs)
+
+
+ListTypes = (List, SimpleList)
 
 
 def Mapping(field_type, **kwargs):
     """A helper "class" to choose the right list class for a given field."""
-    from .bases import MultivaluedField, ProxyField
-    from .multivalued import SimpleMapping
-    from .complex import Mapping
     if isinstance(field_type, ProxyField):
         return Mapping(field_type, **kwargs)
     return SimpleMapping(field_type, **kwargs)
+
+
+MappingTypes = (Mapping, SimpleMapping)
---
+++ draughts/util.py
@@ -0,0 +1,66 @@
+import typing
+from .fields import ListTypes, Compound
+import collections.abc
+
+
+def recursive_update(d: typing.Dict, u: typing.Mapping) -> typing.Union[typing.Dict, typing.Mapping]:
+    if d is None:
+        return u
+
+    if u is None:
+        return d
+
+    for k, v in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = recursive_update(d.get(k, {}), v)
+        else:
+            d[k] = v
+
+    return d
+
+
+def _construct_field(field, value):
+    if isinstance(field, ListTypes):
+        clean, dropped = [], []
+        for item in value:
+            _c, _d = _construct_field(field.field, item)
+            if _c is not None:
+                clean.append(_c)
+            if _d is not None:
+                dropped.append(_d)
+        return clean or None, dropped or None
+
+    elif isinstance(field, Compound):
+        _c, _d = construct_safe(field.model, value)
+        if len(_d) == 0:
+            _d = None
+        return _c, _d
+    else:
+        try:
+            return field.check(value), None
+        except (ValueError, TypeError) as _:
+            return None, value
+
+
+def construct_safe(mod, data) -> typing.Tuple[typing.Any, typing.Dict]:
+    if not isinstance(data, dict):
+        return None, data
+    fields = mod.fields()
+    clean = {}
+    dropped = {}
+    for key, value in data.items():
+        if key not in fields:
+            dropped[key] = value
+            continue
+
+        _c, _d = _construct_field(fields[key], value)
+
+        if _c is not None:
+            clean[key] = _c
+        if _d is not None:
+            dropped[key] = _d
+
+    try:
+        return mod(clean), dropped
+    except ValueError as _:
+        return None, recursive_update(dropped, clean)
